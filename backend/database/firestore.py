984ff1953a4606eba91e0ae5aa4c49f72b0c5bd5
yogi03 <yogendrachaurasiya30@gmail.com>
2026-03-14 15:13:17 +0530

    handle service account key json file

--- backend/database/firestore.py
+++ backend/database/firestore.py
@@ -5,11 +5,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+db = None
+bucket = None
+
+
 # Initialize Firebase (Requires serviceAccountKey.json in production)
 cred_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_PATH", "serviceAccountKey.json")
 
 try:
     if not firebase_admin._apps:
+        cred_dict = {
+            "type": "service_account",
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_CERT_ID"),
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_ID"),
+            "universe_domain": "googleapis.com"
+        }
         cred = credentials.Certificate(cred_path)
         firebase_admin.initialize_app(cred, {
             'storageBucket': os.getenv("FIREBASE_STORAGE_BUCKET", "testify-app.appspot.com")
